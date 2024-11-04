ed73c1c614fb8034cdcf42bd0b78f8e6e6ae5f83
Dr_Drouse <dr.ykov@yandex.ru>
2024-11-04 11:26:39 +1000

    cassette_player v0.0.1

---
+++ app/src/main/java/com/example/cassette_player/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.cassette_player
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Устанавливаем полноэкранный режим
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
+        // Если нужно снова сделать видимой шторку, этот код можно убрать или обернуть в условие.
+    }
+}
