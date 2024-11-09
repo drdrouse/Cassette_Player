fe7a37f67fc7452e9ac60d7de965241f1cd3178a
Dr_Drouse <dr.ykov@yandex.ru>
2024-11-09 21:33:12 +1000

    Music_Menu_v0.3_Landscape

--- app/src/main/java/com/example/cassette_player/SongSelectionActivity.kt
+++ app/src/main/java/com/example/cassette_player/SongSelectionActivity.kt
@@ -12,31 +12,27 @@ import androidx.appcompat.app.AppCompatActivity
 class SongSelectionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_song_selection)
 
+        // Скрыть статус-бар
         getWindow().setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_selection)
 
-        // Пример списка песен
         val songList = listOf("Song 1", "Song 2", "Song 3", "Song 4")
 
-        // Настройка ListView
-        val listView = findViewById<ListView>(R.id.song_list)
+        val listView = findViewById<ListView>(R.id.song_recycler_view)
         val adapter = ArrayAdapter(this, R.layout.list_item_song, R.id.song_title, songList)
         listView.adapter = adapter
 
-        // Обработка нажатия на элемент списка
         listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
             val selectedSong = songList[position]
-
-            // Возвращаем результат в MainActivity
             val resultIntent = Intent()
             resultIntent.putExtra("selected_song", selectedSong)
             setResult(Activity.RESULT_OK, resultIntent)
-            finish() // Закрываем SongSelectionActivity после выбора
+            finish()
         }
     }
 }
